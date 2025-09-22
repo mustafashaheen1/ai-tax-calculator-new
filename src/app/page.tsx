2c42d393ef1f98263fbef93293bf8a41e4e45d71
Mustafa Shaheen <mustafashaheen@ROADSTARs-MacBook-Air.local>
2025-09-22 11:55:42 -0400

    new project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import App from '../components/App'
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Test Page</h1>
-      <p>If you can see this, routing works!</p>
-    </div>
-  )
+  return <App />
 }
